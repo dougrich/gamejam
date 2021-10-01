1d1115bfda9d05b6576fbb0ed66fabbd02fadfa3
dougrich <dougrich@users.noreply.github.com>
2021-10-01 13:02:14 -0700

    Added clear options, drawImage to renderer-gba

--- packages/renderer-gba/renderer-gba.js
+++ packages/renderer-gba/renderer-gba.js
@@ -2,6 +2,33 @@
 const GBA_WIDTH = 240
 const GBA_HEIGHT = 160
 
+function bound (min, max, v) {
+  return Math.max(min, Math.min(max, v))
+}
+
+function loop (top, left, bottom, right, cb) {
+  top = bound(0, GBA_HEIGHT, top)
+  bottom = bound(0, GBA_HEIGHT, bottom)
+  left = bound(0, GBA_WIDTH, left)
+  right = bound(0, GBA_WIDTH, right)
+  for (let j = top; j < bottom; j++) {
+    for (let i = left; i < right; i++) {
+      cb(i, j)
+    }
+  }
+}
+function idx (width, x, y) {
+  return (y * width + x) * 4
+}
+
+function blendPixel (src, dest, srcIdx, destIdx) {
+  if (src[srcIdx + 3] > 128) {
+    dest[destIdx + 0] = src[srcIdx + 0]
+    dest[destIdx + 1] = src[srcIdx + 1]
+    dest[destIdx + 2] = src[srcIdx + 2]
+  }
+}
+
 function createRendererGBA (depends, config) {
   const { canvas } = depends.on('canvas')
 
@@ -10,13 +37,36 @@ function createRendererGBA (depends, config) {
   const data = imageData.data
 
   const r = {
-    clear: () => {
-      for (let i = 0; i < data.length; i += 4) {
-        data[i] = 0
-        data[i + 1] = 0
-        data[i + 2] = 0
-        data[i + 3] = 255
+    clear: (options = {}) => {
+      if (options.gradient) {
+        const [from, to] = options.gradient
+        loop(0, 0, GBA_HEIGHT, GBA_WIDTH, (x, y) => {
+          const distance = y / GBA_HEIGHT
+          const blended = [0, 0, 0].map((_, i) => ((Math.floor(from[i] * (1 - distance) + to[i] * distance)) >> 3) << 3)
+          blended.push(255)
+          blendPixel(blended, data, 0, idx(GBA_WIDTH, x, y))
+          data[idx(GBA_WIDTH, x, y) + 3] = 255
+        })
+      } else {
+        let color = [0, 0, 0]
+        if (options.color) {
+          color = options.color
+        }
+        for (let i = 0; i < data.length; i += 4) {
+          data[i] = color[0]
+          data[i + 1] = color[1]
+          data[i + 2] = color[2]
+          data[i + 3] = 255
+        }
       }
+      return r
+    },
+    drawImage: (img, { srcX, srcY, destX, destY, W, H }) => {
+      loop(destY, destX, destY + H, destX + W, (i, j) => {
+        const srcIdx = idx(img.width, i - destX, j - destY)
+        const destIdx = idx(GBA_WIDTH, i, j)
+        blendPixel(img.data, data, srcIdx, destIdx)
+      })
       return r
     },
     flush: () => {
--- packages/renderer-gba/renderer-gba.test.js
+++ packages/renderer-gba/renderer-gba.test.js
@@ -1,4 +1,6 @@
 const { toMatchImageSnapshot } = require('jest-image-snapshot')
+const fs = require('fs')
+const { PNG } = require('pngjs')
 
 const { GBA_WIDTH, GBA_HEIGHT, createRendererGBA } = require('./renderer-gba')
 
@@ -6,10 +8,57 @@ expect.extend({ toMatchImageSnapshot })
 
 const createCanvas = () => require('canvas').createCanvas(GBA_WIDTH, GBA_HEIGHT)
 
-test('clear', () => {
+const loadImage = (path) => new Promise((resolve, reject) => {
+  fs.createReadStream(path)
+    .pipe(new PNG())
+    .on('parsed', function () {
+      resolve({ width: this.width, height: this.height, data: this.data })
+    })
+    .on('error', function (error) {
+      reject(error)
+    })
+})
+
+test('clear - simple', () => {
   const canvas = createCanvas()
   createRendererGBA({ on: () => ({ canvas }) })
     .clear()
+    .flush()
+  const image = canvas.toBuffer('image/png')
+  expect(image).toMatchImageSnapshot()
+})
+
+test('clear - color', () => {
+  const canvas = createCanvas()
+  createRendererGBA({ on: () => ({ canvas }) })
+    .clear({ color: [255, 0, 0] })
+    .flush()
+  const image = canvas.toBuffer('image/png')
+  expect(image).toMatchImageSnapshot()
+})
+
+test('clear - gradient', () => {
+  const canvas = createCanvas()
+  createRendererGBA({ on: () => ({ canvas }) })
+    .clear({ gradient: [[255, 0, 0], [0, 255, 0]] })
+    .flush()
+  const image = canvas.toBuffer('image/png')
+  expect(image).toMatchImageSnapshot()
+})
+
+test('drawImage - simple', async () => {
+  const canvas = createCanvas()
+  const img = await loadImage('test/smile.png')
+  createRendererGBA({ on: () => ({ canvas }) })
+    .clear()
+    .drawImage(img, {
+      srcX: 0,
+      srcY: 0,
+      destX: 32,
+      destY: 32,
+      W: 32,
+      H: 32
+    })
     .flush()
   const image = canvas.toBuffer('image/png')
   expect(image).toMatchImageSnapshot()
